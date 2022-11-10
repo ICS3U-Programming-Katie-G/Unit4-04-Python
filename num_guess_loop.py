9e72476d5d86006c916b8884877379f5679791de
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2022-11-10 17:43:55 +0000

    finised code for number guessing program using a loop and break statements in python.

---
+++ num_guess_loop.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# Created by: Katie G
+# Created on: November 8th, 2022
+# This program generates a random number, then uses a
+# # do...while loop to get the user number, uses a try...catch statement
+# to check if the user input is an integer, then an if...then
+# statement to check if the user's input is within the range of 0-9.
+# the program then uses an if...then statement to check if the user guess
+# is equal to the generated number - if so, uses a break statement to break
+# out of the loop.
+import random
+
+
+# this function will generate a random number, then, while using a do...while loop,
+# uses a try...catch statement and an if...then statement to check if the user
+# guess is valid, then uses an if...then statement to check if the user guess
+# is equal to the generated num - if so, uses a "break" statement to break
+# out of the loop.
+def main():
+    # generates a random number from 0-9 and assigns it
+    # to the random number variable.
+    random_num = random.randint(0, 9)
+
+    # the beginning of the do... while loop that the rest of
+    # the program will be contained in.
+    while True:
+        user_guess_as_string = input(
+            "I am thinking of a number from 0-9. You will guess what it is please. "
+        )
+        try:
+            user_guess_as_int = int(user_guess_as_string)
+            if user_guess_as_int >= 0 and user_guess_as_int <= 9:
+                if user_guess_as_int == random_num:
+                    print("You guessed correctly! Well done!")
+                    break
+                else:
+                    print("You have not guessed correctly. Try again.")
+            else:
+                print("Please enter a whole, positive number from 0-9.")
+        except Exception:
+            print("Please enter a valid integer from 0-9.")
+
+
+if __name__ == "__main__":
+    main()
